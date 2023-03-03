23ed67b22dd06589f8a28a146628a2d532e9758a
hagrace4 <hagrace4@gmail.com>
2023-03-03 10:24:46 -0800

    add express dependencies

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
+//add dependencies for express
+let express = require("express");
+let app = express();
+let bodyParser = require("body-parser");
+let http = require("http").Server(app);
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true}));
 
-
+//add dependency for mongoDB client
+//retrieve mongoDB username and password from env variables
+//set const uri to cluster
 const MongoClient = require("mongodb").MongoClient;
 const mongo_username = process.env["MONGO_USERNAME"];
 const mongo_password = process.env["MONGO_PASSWORD"];
 
 const uri = `mongodb+srv://${mongo_username}:${mongo_password}@valibtc-crm-db.7fgtoke.mongodb.net/crmdb?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true });
+
