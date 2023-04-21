9a40b80fa5715ff465c664cbfaa33cf04eaa3df7
Harry Grace <36256262+hagrace4@users.noreply.github.com>
2023-04-21 08:41:49 -0700

    Add User Model

---
+++ passport-config.js
@@ -0,0 +1,25 @@
+const JwtStrategy = require('passport-jwt').Strategy;
+const ExtractJwt = require('passport-jwt').ExtractJwt;
+const passport = require('passport');
+const User = require('./models/User'); // You will create this file in the next step
+
+const opts = {
+  jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+  secretOrKey: process.env.JWT_SECRET,
+};
+
+passport.use(
+  new JwtStrategy(opts, async (jwt_payload, done) => {
+    try {
+      const user = await User.findOne({ _id: jwt_payload.sub });
+      if (user) {
+        return done(null, user);
+      }
+      return done(null, false);
+    } catch (error) {
+      return done(error, false);
+    }
+  })
+);
+
+module.exports = passport;
